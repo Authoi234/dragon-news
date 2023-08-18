b7665a3d4a9eb5e94dfccc8cd0ef26b9f819090e
Authoi234 <jawadkhan.authoi@gmail.com>
2023-08-18 12:34:53 +0600

    folders are ready and header also

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <RouterProvider routes={routes}></RouterProvider>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
